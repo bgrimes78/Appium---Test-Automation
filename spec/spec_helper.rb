d1c6b1e088dc8b91f2471a507ad566ab99258b02
bgrimes78 <brandon.grimes@willowtreeapps.com>
2015-04-24 13:31:27 -0400

    updates for iOS support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,14 +14,20 @@ if (ConfigFile["ios"])
   def desired_caps
     {
       caps: {
+        # For iPhone Simulator, device name is 'iPhone #', for testing on device, just type 'iPhone'
         deviceName: "iPhone",
+        
+        # UDID only needed for testing on device
         udid: "add udid here",
+        
         platformName: "iOS",
+        
+        # The OS version only matters when you are testing on Simulator and is dictated by the version of XCode you have installed, which always defaults to the latest version of iOS
         platformVersion: "8.2",
         
-        #Comment out bundleID or app depending on how you are testing
+        # Comment out bundleID or app depending on how you are testing
 
-        # Bundle ID needed when running on CI (Jenkins) as app will already be installed by fruitstrap
+        # Bundle ID needed when running on CI (Jenkins) as app will already be installed by fruitstrap.  Bundle ID can be left in for testing on device.
         bundleId: "com.app.myapp",
         
         # App is for local testing outside of CI as appium will install the app on its own
