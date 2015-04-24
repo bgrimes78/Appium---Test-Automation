fa28e6992daee2c3636172c2699b79d1e19bc498
bgrimes78 <brandon.grimes@willowtreeapps.com>
2015-04-24 11:14:25 -0400

    add swipe

--- lib/PageObjects/ExamplePageObjects.rb
+++ lib/PageObjects/ExamplePageObjects.rb
@@ -12,5 +12,22 @@ class Example
 		complex_find_exact('android.widget.TextView', Example_Text)
 		complex_find_exact('android.widget.Button', Example_Button)
 	end
+	
+	#SWIPE on IOS
+	
+	def swiperightiOS(a, b, c, d, e)
+		swipe(:start_x => a, :start_y => b, :end_x => c, :end_y => d, :duration => e).perform
+	end
+
+	def swipeleftiOS(a, b, c, d, e)
+		swipe(:start_x => a, :start_y => b, :end_x => c, :end_y => d, :duration => e).perform
+	end
+	
+	def swipepages
+		swiperightiOS(10, 100, 100, 100, 500)
+		#verify the screen
+		swipeleftiOS(100, 100, 10, 100, 500)
+		#verify the screen
+	end
 
 end
